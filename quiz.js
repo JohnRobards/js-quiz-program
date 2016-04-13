18eb67e6f8d0d9699a6dcd4f56e6367c66c63037
JohnRobards <jrobards@wustl.edu>
2016-04-13 18:18:45 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,12 +2,39 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
+var answer1 = prompt("What was the first name of Lincoln?");
+var answer2 = prompt("What was the first name of the first president of the US?");
+var answer3 = prompt("What color is a banana?");
+var answer4 = prompt("What company makes Dasani water?");
+var answer5 = prompt("What animal goes 'Meow'?");
 
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'ABRAHAM' ) {
+ correct += 1;
+}
+if ( answer2.toUpperCase() === 'GEORGE' ) {
+ correct += 1;
+}
+if ( answer3.toUpperCase() === 'YELLOW' ) {
+ correct += 1;
+}
+if ( answer4.toUpperCase() === 'COKE' ) {
+ correct += 1;
+}
+if ( answer5.toUpperCase() === 'CAT' ) {
  correct += 1;
 }
 
 // output results
-document.write("<p>write something to page...</p>");
+var medal = "";
+if (correct == 5) {
+  medal += "gold";
+}
+else if (correct >= 3) {
+  medal += "silver";
+}
+else {
+ medal += "bronze";
+}
+document.write(correct + " ouf of 5 correct, you got the " + medal + " medal!");
+
